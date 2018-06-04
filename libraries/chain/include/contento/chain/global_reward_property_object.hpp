7b30c447bad6b2cfddff4c94d05afbef4f2e93ba
Asa17 <zengli@gmail.com>
2018-06-04 20:53:14 +0800

    eco3

---
+++ libraries/chain/include/contento/chain/global_reward_property_object.hpp
@@ -0,0 +1,12 @@
+//
+//  global_reward_property_object.hpp
+//  Steem
+//
+//  Created by Sanae Aprocy on 2018/6/4.
+//
+
+#ifndef global_reward_property_object_h
+#define global_reward_property_object_h
+
+
+#endif /* global_reward_property_object_h */
