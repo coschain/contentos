f298036b147101cd06ac50fde7147b3a0f8468e5
huoxin <nwpudonkey@gmail.com>
2018-08-13 19:52:50 +0800

    compile error

--- libraries/app/include/contento/app/contento_api_objects.hpp
+++ libraries/app/include/contento/app/contento_api_objects.hpp
@@ -218,8 +218,8 @@ struct account_api_obj
       witnesses_voted_for( a.witnesses_voted_for ),
       last_post( a.last_post ),
       last_root_post( a.last_root_post ),
-      code( a.code ),
-      abi( a.abi )
+      code( to_string(a.code) ),
+      abi( to_string(a.abi) )
    {
       size_t n = a.proxied_vsf_votes.size();
       proxied_vsf_votes.reserve( n );
