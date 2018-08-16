9668dc94512b4b794f787607bd56e5228f1c1fc6
daishuo <daishuo@gmail.com>
2018-08-16 15:51:47 +0800

    bugfix: do not call price functions when wasm vm raises exceptions

--- externals/binaryen/src/support/scope_exit.h
+++ externals/binaryen/src/support/scope_exit.h
@@ -8,18 +8,23 @@ namespace wasm {
 
     struct scope_exit {
         
-        scope_exit( std::function<void()>&& func ) : _func(std::move(func)) {
+        scope_exit( std::function<void()>&& func ) : _func(std::move(func)), _enabled(true) {
             
         }
         
         ~scope_exit() {
-            _func();
+            if (_enabled) _func();
+        }
+        
+        void enable(bool b) {
+            _enabled = b;
         }
         
         scope_exit(const scope_exit&) = delete;
         scope_exit& operator=(const scope_exit&) = delete;
         
         std::function<void()> _func;
+        bool _enabled;
     };
 
 } // namespace wasm
