dd06935afae137d9a98d59f9cf9d6767972ada75
huoxin <nwpudonkey@gmail.com>
2018-09-03 12:40:52 +0800

    judgement exist before create contract_balance_object

--- libraries/chain/contento_contract.cpp
+++ libraries/chain/contento_contract.cpp
@@ -83,10 +83,15 @@ void apply_contento_setabi(apply_context& context) {
          memcpy( (void*)a.abi.data(), act.abi.data(), abi_size );
    });
     
-    db.create< contract_balance_object > ([&]( auto& cbo ) {
-        // here is system contract, contract_name is system pre-defined, actual target contract name is in the caller
-        cbo.contract_name = context.op.caller;
-    } );
+     const auto& by_bank_name_idx = db.get_index< contract_balance_index >().indices().get< by_name >();
+    auto bank_itr = by_bank_name_idx.find( context.op.caller );
+    if(bank_itr == by_bank_name_idx.end()){
+        db.create< contract_balance_object > ([&]( auto& cbo ) {
+            // here is system contract, contract_name is system pre-defined, actual target contract name is in the caller
+            cbo.contract_name = context.op.caller;
+        } );
+    }
+   
     /* TODOO:
    const auto& account_sequence = db.get<account_sequence_object, by_name>(act.account);
    db.modify( account_sequence, [&]( auto& aso ) {
