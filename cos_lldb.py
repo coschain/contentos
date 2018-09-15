2b9800d7a6160a8e40f74fea06fbfe353681bd5f
daishuo <daishuo@gmail.com>
2018-09-16 02:13:29 +0800

    cos_lldb.py: refactor & better support for fc::variants

--- cos_lldb.py
+++ cos_lldb.py
@@ -1,270 +1,292 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class Helper:
-	handlers = {}
+# This script is lldb data formatter for Contentos C++ project.
+# Install it by running it. ~/.lldbinit will be modified so that lldb loads formatters when launching.
 
-	@staticmethod
-	def add_handler(clazz_name, handler_clazz):
-		Helper.handlers[clazz_name] = handler_clazz
+import re
 
-	@staticmethod
-	def apply(debugger):
-		for t, c in Helper.handlers.iteritems():
-			c.apply(debugger, t)
+######## helper ########
 
+class Helper(object):
+
+	# get the std namespace 
 	@staticmethod
-	def get_std_namespace(target):
-		result = None
+	def std_namespace(target):
+		r = None
 		for ns in ["std", "std::__1"]:
 			try:
-				t = target.CreateValueFromExpression("_", "(%s::string*)0" % ns).type
+				if target.CreateValueFromExpression("_", "(%s::string*)0" % ns).type:
+					r = ns[:]
+					break
 			except:
-				t = None
-			if t:
-				result = ns[:]
-				break
-		return result
-
-
-######## type formatters ########
-
-
-class fc__variant:
-
-	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand("type synthetic add %s --python-class %s.%s" % (target_class_name, clz.__module__, clz.__name__))
-		debugger.HandleCommand('type summary add --summary-string "${var.value}" %s' % target_class_name)
-
-	def __init__(self, valobj, internal_dict):
-		self.valobj = valobj
-		self.val = None
-		std_ns = Helper.get_std_namespace(valobj.target)
-		self.types = ["long long", "unsigned long long", "double", "bool", 
-			"%s::string*" % std_ns, 
-			"fc::variants*", 
-			"fc::variant_object*",
-			"fc::blob*"
-			]
-		self.parse()
-
-	def parse(self):
-		typeid = 0
-		try:
-			typeid = self.valobj.GetChildMemberWithName("_type").data.sint8s[-1]
-			val = self.valobj.GetChildMemberWithName("_data")
-		except:
-			typeid = 0
-		if 0 < typeid < 9:
-			real_type = self.valobj.CreateValueFromExpression("_tmp_ptr_", "(%s)0" % self.types[typeid - 1]).type
-			real_val = self.valobj.CreateValueFromData("_tmp_val_", val.data, real_type)
-			while real_val.TypeIsPointerType():
-				real_val = real_val.deref
-			self.val = self.valobj.target.CreateValueFromData("value", real_val.data, real_val.type)
-		elif typeid == 0:
-			self.val = self.valobj.target.CreateValueFromExpression("value", "(void*)0")
-
-	def get_value(self):
-		return self.val
-
-	def num_children(self):
-		return 1 if self.val else 0
-
-	def has_children(self):
-		return self.num_children() > 0
-
-	def get_child_index(self, name):
-		return 0 if name == "value" else None
-
-	def get_child_at_index(self, index):
-		return self.val if index == 0 else None
-
-	def update(self):
-		if self.valobj.changed:
-			self.parse()
-
-
-class fc__variant_object:
-	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand("type synthetic add %s --python-class %s.%s" % (target_class_name, clz.__module__, clz.__name__))
-		debugger.HandleCommand('type summary add --summary-string "size=${var%%#}" %s' % target_class_name)
-
-	def __init__(self, valobj, internal_dict):
-		self.valobj = valobj
-		self.count = 0
-		self.entries = []
-		self.parse()
-		
-	def parse(self):
-		self.count, self.entries = 0, []
-		if self.valobj.IsValid() and self.valobj.size > 0:
-			vec_ptr = self.valobj.GetChildMemberWithName("_key_value").GetChildMemberWithName("__ptr_")
-			if vec_ptr.IsValid() and vec_ptr.value:
-				entry_type = "fc::variant_object::entry"
-				begin = self.valobj.target.CreateValueFromExpression("begin", "*(%s**)%s" % (entry_type, vec_ptr.value))
-				end = self.valobj.target.CreateValueFromExpression("end", "*(%s**)(%s + sizeof(void*))" % (entry_type, vec_ptr.value))
-				count = self.valobj.target.CreateValueFromExpression("count", "(%s - %s) / sizeof(%s)" % (end.value, begin.value, entry_type)).signed
-				self.count = min(max(count, 0), 1024)
-				self.entries = map(lambda i: begin.GetValueForExpressionPath("[%d]" % i), range(self.count))
-
-	def num_children(self):
-		return self.count
-
-	def has_children(self):
-		return self.num_children() > 0
-
-	def get_child_index(self, name):
-		r = None
-		if name.startswith("[") and name.endswith("]"):
-			r = int(name[1:-1])
-			if r >= self.count:
-				r = None
+				pass
 		return r
 
-	def get_child_at_index(self, index):
-		return self.entries[index] if index < self.count else None
 
-	def update(self):
-		if self.valobj.changed:
-			self.parse()
+######## formatter bases ########
 
 
-class fc__variant_object__entry:
+# base class for all formatters.
+class baseFormatter(object):
 	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand('type summary add --inline-children %s' % target_class_name)
+	def apply(clz, debugger, target_clazz_name):
+		raise NotImplementedError
 
 
-class contento__protocol__operation:
+# base class for formatters that change summary string only.
+class baseSummaryFormatter(baseFormatter):
 	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand("type synthetic add %s --python-class %s.%s" % (target_class_name, clz.__module__, clz.__name__))
-		debugger.HandleCommand("type synthetic add --python-class %s.%s -x \"^fc::static_variant<.+>$\"" % (clz.__module__, clz.__name__))
-		debugger.HandleCommand("type synthetic add --python-class %s.%s -x \"^const fc::static_variant<.+>$\"" % (clz.__module__, clz.__name__))
+	def apply(clz, debugger, target_clazz_name):
+		debugger.HandleCommand("type summary add --python-function %s.%s.summary %s" % (clz.__module__, clz.__name__, target_clazz_name))
+
+	# override this and return the summary string based on given value.
+	@staticmethod
+	def summary(valobj, internal_dict):
+		raise NotImplementedError
+
+
+# base class for formatters that create synthetic children.
+class baseSyntheticFormatter(baseFormatter):
+	@classmethod
+	def apply(clz, debugger, target_clazz_name):
+		debugger.HandleCommand("type synthetic add %s --python-class %s.%s" % (target_clazz_name, clz.__module__, clz.__name__))
+	
+	# deal with special case of index comparasion. "[10]" must be greater than "[2]".
+	@staticmethod
+	def name_cmp(name1, name2):
+		idx_pat = re.compile(r"^\[\d+\]$")
+		if idx_pat.match(name1) and idx_pat.match(name2):
+			return cmp(int(name1[1:-1]), int(name2[1:-1]))
+		else:
+			return cmp(name1, name2)
 
 	def __init__(self, valobj, internal_dict):
 		self.valobj = valobj
-		self.val = None
-		self.parse()
-		
+		self.children_names = list()
+		self.children = dict()
+		self.ever_parsed = False
+	
+	# override this and fill in self.children dict.
 	def parse(self):
-		if self.valobj.IsValid() and self.valobj.size > 0:
-			t = self.valobj.type
-			self.real_parse(t.GetTypedefedType() if t.IsTypedefType() else t)
+		pass
 
-	def real_parse(self, t):
-		self.val = None
-		idx = self.valobj.GetChildMemberWithName("_tag").signed
-		sv_typename = t.name
-		pos = sv_typename.find("fc::static_variant<")
-		if pos >= 0 and sv_typename.endswith(">"):
-			types = map(str.strip, sv_typename[pos + 19:-1].split(","))
-			if 0 <= idx < len(types):
-				op_type = self.valobj.CreateValueFromExpression("_tmp_ptr_", "(%s*)0" % types[idx]).type.GetPointeeType()
-				self.val = self.valobj.CreateValueFromData("value", self.valobj.data, op_type)
-
-	def get_value(self):
-		return self.val
-
+	def is_valid(self):
+		return self.valobj.IsValid() and self.valobj.size > 0
+	
 	def num_children(self):
-		return 1 if self.get_value() else 0
+		return len(self.children_names)
 
 	def has_children(self):
 		return self.num_children() > 0
 
 	def get_child_index(self, name):
-		return 0 if name == "value" else None
+		return self.children_names.index(name)
 
 	def get_child_at_index(self, index):
-		return self.val if index == 0 else None
+		return self.children.get(self.children_names[index])
 
 	def update(self):
-		if self.valobj.changed:
-			self.parse()
+		need_parse = self.valobj.changed or not self.ever_parsed
+		print "update called.", need_parse
+		if need_parse:
+			self.children = dict()
+			if self.is_valid():
+				self.parse()
+			self.children_names = sorted(self.children.keys(), cmp=baseSyntheticFormatter.name_cmp)
+			self.ever_parsed = True
+		return need_parse
 
 
-class contento__protocol__asset:
+# base class for formatters suitable with std::vector-like value types.
+class baseArrayFormatter(baseSyntheticFormatter):
 	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand('type summary add --python-function %s.%s.asset_summary %s' % (clz.__module__, clz.__name__, target_class_name))
+	def apply(clz, debugger, target_clazz_name):
+		super(baseArrayFormatter, clz).apply(debugger, target_clazz_name)
+		prefix = clz.summary_prefix()
+		debugger.HandleCommand('type summary add --summary-string "%s size=${var%%#}" %s' % (prefix, target_clazz_name))
 
+	# override this and return the prefix of summary string.
+	# summary string will be like "${prefix} size=xxx"
+	@classmethod
+	def summary_prefix(clz):
+		return "array"
+	
+	# override this and return the pointer value of the first element.
+	def begin_pointer(self):
+		return None
+
+	# override this and return the pointer value of the last+1 element.
+	def end_pointer(self):
+		return None
+
+	def parse(self):
+		try:
+			begin, end = self.begin_pointer(), self.end_pointer()
+			element_type = begin.type.GetPointeeType()
+			element_size = element_type.GetByteSize()
+			if begin.IsValid() and end.IsValid() and element_size > 0:
+				count = self.valobj.CreateValueFromExpression("_count_", "(%s-%s)/%d" % (end.value, begin.value, element_size)).signed
+				if count > 0:
+					for i in xrange(count):
+						val_name = "[%d]" % i
+						self.children[val_name] = begin.CreateChildAtOffset(val_name, i * element_size, element_type)
+		except:
+			pass
+
+
+######## formatters ########
+
+class fc__variant(baseSyntheticFormatter):
+	@classmethod
+	def apply(clz, debugger, target_clazz_name):
+		super(fc__variant, clz).apply(debugger, target_clazz_name)
+		debugger.HandleCommand('type summary add --summary-string "${var.value}" %s' % target_clazz_name)
+
+	def __init__(self, valobj, internal_dict):
+		super(fc__variant, self).__init__(valobj, internal_dict)
+		std = Helper.std_namespace(valobj.target)
+		self.sub_types = [
+			"void*",
+			"long long",
+			"unsigned long long",
+			"double",
+			"bool",
+			std + "::string*",
+			"fc::variants*",
+			"fc::variant_object*",
+			"fc::blob*",
+		]
+
+	def parse(self):
+		typeid = self.valobj.GetChildMemberWithName("_type").data.sint8s[-1]
+		val = self.valobj.GetChildMemberWithName("_data")
+		if 0 <= typeid < len(self.sub_types):
+			subtype = self.sub_types[typeid]
+			if typeid > 0:
+				real_type = self.valobj.CreateValueFromExpression("_tmp_", "(%s)0" % subtype).type
+				real_val = self.valobj.CreateValueFromData("_tmp_val_", val.data, real_type)
+				while real_val.TypeIsPointerType():
+					real_val = real_val.deref
+				self.children["value"] = self.valobj.CreateValueFromData("value", real_val.data, real_val.type)
+			else:
+				self.children["value"] = self.valobj.CreateValueFromExpression("value", "nullptr")
+
+
+# fc::variants is just a typedef of std::vector<fc::variant>.
+# it should be supported by built-in formatters for std::vector, but lldb sometimes can't handle fc::variants properly.
+# so that we have to add an explicit formatter here.
+class fc__variants(baseArrayFormatter):
+	def begin_pointer(self):
+		return self.valobj.GetChildMemberWithName("__begin_")
+
+	def end_pointer(self):
+		return self.valobj.GetChildMemberWithName("__end_")
+
+
+# fc::variant_object's _key_value is an std::shared_ptr<std::vector<fc::variant_object::entry>>
+class fc__variant_object(baseArrayFormatter):
+	@classmethod
+	def summary_prefix(clz):
+		return "object"
+
+	def begin_pointer(self):
+		return self.valobj.GetChildMemberWithName("_key_value").GetChildMemberWithName("__ptr_").GetChildMemberWithName("__begin_")
+
+	def end_pointer(self):
+		return self.valobj.GetChildMemberWithName("_key_value").GetChildMemberWithName("__ptr_").GetChildMemberWithName("__end_")
+
+
+class fc__variant_object__entry(baseFormatter):
+	@classmethod
+	def apply(clz, debugger, target_clazz_name):
+		debugger.HandleCommand('type summary add --inline-children %s' % target_clazz_name)
+
+
+# fc::blob { std::vector<char> data; }
+class fc__blob(baseFormatter):
+	@classmethod
+	def apply(clz, debugger, target_clazz_name):
+		debugger.HandleCommand('type summary add --summary-string "blob ${var.data}" %s' % target_clazz_name)
+
+
+class contento__protocol__asset(baseSummaryFormatter):
 	@staticmethod
-	def asset_summary(valobj, internal_dict):
-		r = ""
-		if valobj.IsValid() and valobj.size > 0:
+	def summary(valobj, internal_dict):
+		r = "[invalid]"
+		try:
 			amount = valobj.GetChildMemberWithName("amount").GetChildMemberWithName("value").data.sint64s[0]
 			symbol = valobj.GetChildMemberWithName("symbol").data.uint64s[0]
-			symbol_name, digits = "", symbol & 0xff
-			divider = 10 ** digits
+			symbol_name, digits = [], symbol & 0xff
 			symbol >>= 8
 			while symbol != 0:
-				symbol_name += chr(symbol & 0xff)
+				symbol_name.append(chr(symbol & 0xff))
 				symbol >>= 8
-			fmts = "%%d.%%0%dd %s" % (digits, symbol_name)
+			fmts = "%%d.%%0%dd %s" % (digits, "".join(symbol_name))
+			divider = 10 ** digits
 			r = fmts % (amount / divider, amount % divider)
+		except:
+			pass
 		return r
 
 
-class contento__protocol__name:
-	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand('type summary add --python-function %s.%s.asset_summary %s' % (clz.__module__, clz.__name__, target_class_name))
+class contento__protocol__name(baseSummaryFormatter):
+	charmap = ".12345abcdefghijklmnopqrstuvwxyz"
 
 	@staticmethod
-	def asset_summary(valobj, internal_dict):
-		charmap = ".12345abcdefghijklmnopqrstuvwxyz"
-		r = ["."] * 13
-		if valobj.IsValid() and valobj.size > 0:
+	def summary(valobj, internal_dict):
+		r = "[invalid]"
+		try:
 			value = valobj.GetChildMemberWithName("value").data.uint64s[0]
+			r = ["."] * 13
 			for i in xrange(13):
-				r[12 - i] = charmap[value & (0x0f if i == 0 else 0x1f)]
+				r[12 - i] = contento__protocol__name.charmap[value & (0x0f if i == 0 else 0x1f)]
 				value >>= 4 if i == 0 else 5
-		return '"%s"' % "".join(r).rstrip(".")
+		except:
+			pass
+		return 'name "%s"' % "".join(r).rstrip(".")
 
 
-class contento__protocol__namex:
-	@classmethod
-	def apply(clz, debugger, target_class_name):
-		debugger.HandleCommand('type summary add --python-function %s.%s.asset_summary %s' % (clz.__module__, clz.__name__, target_class_name))
-
+class contento__protocol__namex(baseSummaryFormatter):
 	@staticmethod
 	def uint64_to_string(ui64):
-		r = list()
-		while ui64 > 0:
-			r.append(chr(ui64 & 0xff))
-			ui64 >>= 8
-		return "".join(r)
+		return "".join(map(chr, [(ui64 >> i) & 0xff for i in xrange(0, 64, 8)])).rstrip("\0")
 
 	@staticmethod
-	def asset_summary(valobj, internal_dict):
-		r = ""
-		if valobj.IsValid() and valobj.size > 0:
+	def summary(valobj, internal_dict):
+		r = "[invalid]"
+		try:
 			lo = valobj.GetChildMemberWithName("value").GetChildMemberWithName("lo").data.uint64s[0]
 			hi = valobj.GetChildMemberWithName("value").GetChildMemberWithName("hi").data.uint64s[0]
+			r = ""
 			if lo > 0:
 				r += contento__protocol__namex.uint64_to_string(lo) + contento__protocol__namex.uint64_to_string(hi)
-		return '"%s"' % r
+		except:
+			pass
+		return 'name "%s"' % r
+
 
 ######## module entry point ########
 
 def __lldb_init_module(debugger, dictionary):
-	Helper.add_handler("fc::variant", fc__variant)
-	Helper.add_handler("fc::variant_object", fc__variant_object)
-	Helper.add_handler("fc::variant_object::entry", fc__variant_object__entry)
-	Helper.add_handler("contento::protocol::operation", contento__protocol__operation)
-	Helper.add_handler("contento::protocol::asset", contento__protocol__asset)
-	Helper.add_handler("contento::protocol::name", contento__protocol__name)
-	Helper.add_handler("contento::protocol::namex", contento__protocol__namex)
-	Helper.apply(debugger)
+	handlers = dict()
+	handlers["fc::variant"] = fc__variant
+	handlers["fc::variants"] = fc__variants
+	handlers["fc::variant_object"] = fc__variant_object
+	handlers["fc::variant_object::entry"] = fc__variant_object__entry
+	handlers["fc::blob"] = fc__blob
+	handlers["contento::protocol::asset"] = contento__protocol__asset
+	handlers["contento::protocol::name"] = contento__protocol__name
+	handlers["contento::protocol::namex"] = contento__protocol__namex
+	
+	for clazz_name, clazz in handlers.iteritems():
+		clazz.apply(debugger, clazz_name)
 
 
 ######## installer ########
 
 if __name__ == "__main__":
 	from os import path
-	import sys
 
 	lldbinit = path.expanduser("~/.lldbinit")
 	me = "command script import %s\n" % path.abspath(__file__)
